6a169da929d088d5f0e1f48cfa7d692e92f0b74f
Mathias Bucher <mathias_bucher@hotmail.com>
2018-02-25 16:49:31 +0100

    add multiple call test

--- cexample/test/test_fibonacci.cpp
+++ cexample/test/test_fibonacci.cpp
@@ -10,6 +10,7 @@
 #include "mocks/mock_MyMath.cpp"
 using ::testing::Exactly;
 using ::testing::Return;
+using ::testing::InSequence;
 
 #include "fibonacci.h"
 
@@ -54,4 +55,31 @@ TEST_F(Test_Fibonacci, getNext_once)
 	.WillOnce(Return(2));
 
 	ASSERT_EQ(fb.getNext(),2);
+}
+
+/**
+ * Test getNext method with multiple calls
+ * @param Test_Fibonacci Test class which contains SetUp and TearDown.
+ * Is also test group name.
+ * @param getNext Test case name.
+ */
+TEST_F(Test_Fibonacci, getNext_multiple)
+{
+	InSequence dummy;
+	MockMyMath mymath_mock;
+	Fibonacci fb(&mymath_mock);
+
+	EXPECT_CALL(mymath_mock, myadd(1,1))
+	.Times(Exactly(1))
+	.WillOnce(Return(2));
+	EXPECT_CALL(mymath_mock, myadd(2,1))
+	.Times(Exactly(1))
+	.WillOnce(Return(3));
+	EXPECT_CALL(mymath_mock, myadd(3,2))
+	.Times(Exactly(1))
+	.WillOnce(Return(5));
+
+	ASSERT_EQ(fb.getNext(),2);
+	ASSERT_EQ(fb.getNext(),3);
+	ASSERT_EQ(fb.getNext(),5);
 }
